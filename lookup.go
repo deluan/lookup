35368481ec3be8a8cc8abb2fd4df43fec2150b26
Deluan <deluan@deluan.com>
2019-09-21 18:24:13 -0400

    New public interface for Lookup

--- lookup.go
+++ lookup.go
@@ -1,16 +1,32 @@
-//  Search for all occurrences of template inside img, using the NCC algorithm.
+// Package lookup implements a nice, simple and fast library which helps you to
+// lookup objects on a screen. It also includes OCR functionality. Using Lookup
+// you can do OCR tricks like recognizing any information in your Robot application.
+//
+// The image search algorithm is based on the Normalized Cross Correlation algorithm.
 package lookup
 
 import "image"
 
-func GrayScale(img image.Image, template image.Image, m float64) ([]GPoint, error) {
-	imgGray := ensureGrayScale(img)
-	templateGray := ensureGrayScale(template)
-	return Color(imgGray, templateGray, m)
+type Lookup struct {
+	imgBin *imageBinary
 }
 
-func Color(img image.Image, template image.Image, m float64) ([]GPoint, error) {
-	imgBin := newImageBinary(img)
-	templateBin := newImageBinary(template)
-	return lookupAll(imgBin, templateBin, m)
+func NewLookup(image image.Image) *Lookup {
+	return &Lookup{
+		imgBin: newImageBinary(image),
+	}
+}
+
+func NewLookupGrayScale(image image.Image) *Lookup {
+	imgGray := ensureGrayScale(image)
+	return NewLookup(imgGray)
+}
+
+//  Search for all occurrences of template inside image
+func (l *Lookup) FindAll(template image.Image, threshold float64) ([]GPoint, error) {
+	if len(l.imgBin.channels) == 1 {
+		template = ensureGrayScale(template)
+	}
+	tb := newImageBinary(template)
+	return lookupAll(l.imgBin, tb, threshold)
 }
--- lookup_test.go
+++ lookup_test.go
@@ -7,14 +7,28 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestLookupAll(t *testing.T) {
-	Convey("Given an Color image and a template to look for", t, func() {
-		img := loadImageColor("testdata/cyclopst1.png")
-		template := loadImageColor("testdata/cyclopst3.png")
+func TestLookup(t *testing.T) {
+	Convey("Given a GrayScale image", t, func() {
+		img := loadImageGray("testdata/cyclopst1.png")
+		l := NewLookupGrayScale(img)
 
-		Convey("When searching", func() {
-			pp, _ := Color(img, template, 0.9)
+		Convey("When I search using a grayscale template", func() {
+			template := loadImageGray("testdata/cyclopst3.png")
+
 			Convey("It finds the template", func() {
+				pp, _ := l.FindAll(template, 0.9)
+				So(pp, ShouldHaveLength, 1)
+				So(pp[0].X, ShouldEqual, 21)
+				So(pp[0].Y, ShouldEqual, 7)
+				So(pp[0].G, ShouldBeGreaterThan, 0.9)
+			})
+		})
+
+		Convey("When I search using a color template", func() {
+			template := loadImageColor("testdata/cyclopst3.png")
+
+			Convey("It finds the template", func() {
+				pp, _ := l.FindAll(template, 0.9)
 				So(pp, ShouldHaveLength, 1)
 				So(pp[0].X, ShouldEqual, 21)
 				So(pp[0].Y, ShouldEqual, 7)
@@ -22,18 +36,31 @@ func TestLookupAll(t *testing.T) {
 			})
 		})
 	})
-	Convey("Given an Gray Scale image and a template to look for", t, func() {
-		img := loadImageGray("testdata/cyclopst1.png")
-		template := loadImageGray("testdata/cyclopst3.png")
 
-		Convey("When searching", func() {
-			pp, _ := GrayScale(img, template, 0.9)
+	Convey("Given a Color image", t, func() {
+		img := loadImageColor("testdata/cyclopst1.png")
+		l := NewLookup(img)
+
+		Convey("When I search using a color template", func() {
+			template := loadImageColor("testdata/cyclopst3.png")
+
 			Convey("It finds the template", func() {
+				pp, _ := l.FindAll(template, 0.9)
 				So(pp, ShouldHaveLength, 1)
 				So(pp[0].X, ShouldEqual, 21)
 				So(pp[0].Y, ShouldEqual, 7)
 				So(pp[0].G, ShouldBeGreaterThan, 0.9)
 			})
 		})
+		Convey("When I search using a grayscale template", func() {
+			template := loadImageGray("testdata/cyclopst3.png")
+
+			Convey("It returns an error", func() {
+				_, err := l.FindAll(template, 0.9)
+				println(err.Error())
+				So(err, ShouldNotBeNil)
+			})
+		})
+
 	})
 }
