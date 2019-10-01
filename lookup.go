6575b7e10e32f758ba46a8ab9865e243f2045de3
Deluan <deluan@deluan.com>
2019-10-01 00:26:02 -0400

    Fix typo

--- lookup.go
+++ lookup.go
@@ -11,7 +11,7 @@ type Lookup struct {
 	imgBin *imageBinary
 }
 
-// Creates a Lookout object. Lookout objects created by these function will do a gray scale search of the templates.
+// Creates a Lookup object. Lookup objects created by these function will do a gray scale search of the templates.
 // Note that if the image or any template is not GrayScale, it will be converted automatically. This could cause some
 // performance hits
 func NewLookup(image image.Image) *Lookup {
@@ -21,7 +21,7 @@ func NewLookup(image image.Image) *Lookup {
 	}
 }
 
-// Creates a Lookout object that works with all color channels of the image (RGB). Keep in mind that Lookout objects
+// Creates a Lookup object that works with all color channels of the image (RGB). Keep in mind that Lookup objects
 // created with this function can only accept color templates as parameters to the FindAll method
 func NewLookupColor(image image.Image) *Lookup {
 	return &Lookup{
